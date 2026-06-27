3828fad2a7837d79c0e6cf2cc46a856d7de293cd
FELIPE SILVA <fj782266@gmail.com>
2026-06-27 16:18:54 -0300

    atualiza migrations

--- test_db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine(
-    "postgresql+psycopg://postgres:felipegk%4018@localhost:5432/clinica"
-)
-with engine.connect() as conn:
-    print("CONECTOU COM SUCESSO")
